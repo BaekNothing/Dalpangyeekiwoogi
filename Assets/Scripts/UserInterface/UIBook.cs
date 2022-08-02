dbb20bbcca5502382c63084aa626ead5644a7068
baeknothing <baeknothing@gmail.com>
2022-08-02 18:37:48 +0900

    add uibook

---
+++ Assets/Scripts/UserInterface/UIBook.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBook : UIPanels
+{
+    public override void Init(List<textFactor> factors = null)
+    {
+        base.Init(factors);
+    }
+    
+}
